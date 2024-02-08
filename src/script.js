268e4a7a9cccfe3350a2816d116c2101eb6331e7
kafinabd <kafinabd@gmail.com>
2024-02-08 13:33:16 +0700

    web anyar

---
+++ src/script.js
@@ -0,0 +1,7 @@
+/** @format */
+
+// tombol
+const tombol = document.querySelector("#tombol");
+tombol.addEventListener("click", function () {
+  tombol.classList.toggle("tombol-active");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        dark: "#0f172a",
+        abu: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
